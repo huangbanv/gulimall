f703a433858e30908076297fc2f9a6b678c735a0
zhangjun <2328432115@qq.com>
2022-05-10 20:24:35 +0800

    yml commit

--- gulimall-order/src/test/java/com/zhangjun/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/zhangjun/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,13 +1,18 @@
 package com.zhangjun.gulimall.order;
 
+import com.zhangjun.gulimall.order.service.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
+    @Autowired
+    OrderService orderService;
     @Test
     void contextLoads() {
+        orderService.payOrder("123",1);
     }
 
 }
